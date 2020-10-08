2cb4030d1d7807fb65106dd7962047ff7d6b46ac
tacirus <andyihana@gmail.com>
2020-10-08 09:34:07 +0300

    git commit

---
+++ src/main/java/webBackend/SpringBootTestTwoApplication.java
@@ -0,0 +1,13 @@
+package webBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestTwoApplication.class, args);
+    }
+
+}
